bf031f5e3c8151c6d5c3d0cbad94c7a2c75c4a9c
ign-is <igna.dev.contact@gmail.com>
2025-02-09 01:35:55 -0300

    Add 09-destructor index.php

---
+++ 09-destructor/index.php
@@ -0,0 +1,22 @@
+<?php
+
+class Usuario {
+    public $nombre;
+    public $email;
+
+    public function __construct() {
+        // Esto no se debe hacer
+        // es solo con fines didacticos
+        echo "Creando el objeto <br/>";
+    }
+
+    public function __destruct() {
+        echo "Destruyendo el objeto";
+    }
+}
+
+$usuario = new Usuario(); 
+
+for($i = 0; $i <= 200; $i++) {
+    echo $i."<br/>";
+}
