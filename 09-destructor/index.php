c515c05b342953813fdbe9b06e0337e23268fb32
ign-is <igna.dev.contact@gmail.com>
2025-02-10 00:01:00 -0300

    Add destruct

--- 09-destructor/index.php
+++ 09-destructor/index.php
@@ -5,18 +5,26 @@ class Usuario {
     public $email;
 
     public function __construct() {
+        $this->nombre = "Juan Doe";
+        $this->email = "juandoe@email.com";
         // Esto no se debe hacer
         // es solo con fines didacticos
         echo "Creando el objeto <br/>";
     }
 
+    public function __toString() {
+        return "Hola, {$this->nombre}, estas registrado con {$this->email}";
+    }
+
     public function __destruct() {
-        echo "Destruyendo el objeto";
+        echo "<br/> Destruyendo el objeto";
     }
 }
 
 $usuario = new Usuario(); 
 
-for($i = 0; $i <= 200; $i++) {
-    echo $i."<br/>";
-}
+// for($i = 0; $i <= 200; $i++) {
+//     echo $i."<br/>";
+// }
+
+echo $usuario;
